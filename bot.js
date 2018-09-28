0bfd7279845132e0111a6c245a1c2eb84b5bc8e2
abdobenhamidou15 <43121955+abdobenhamidou15@users.noreply.github.com>
2018-09-28 21:49:56 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,200 @@
+const Discord = require('discord.js');
+const client = new Discord.Client(); 
+
+
+
+
+
+client.on("voiceStateUpdate", (message, new1) => {
+
+  var channel = "450356320437141514";
+  var role = "Black squad"
+  set(message,new1,channel,role);
+});
+
+function set(o,n,channel,role){
+  if (!o.voiceChannel && n.voiceChannel) {
+    if (n.voiceChannelID == channel) {
+        n.addRole(n.guild.roles.find("name", role));
+    };
+
+  } else if (o.voiceChannel && !n.voiceMove) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+
+
+
+} else if (o.voiceChannel && !n.voiceChannel) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+    }
+}
+}
+}
+
+client.on("voiceStateUpdate", (message, new1) => {
+
+  var channel = "450717457230069761";
+  var role = "Online"
+  set(message,new1,channel,role);
+});
+
+function set(o,n,channel,role){
+  if (!o.voiceChannel && n.voiceChannel) {
+    if (n.voiceChannelID == channel) {
+        n.addRole(n.guild.roles.find("name", role));
+    };
+
+  } else if (o.voiceChannel && !n.voiceMove) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+
+
+
+} else if (o.voiceChannel && !n.voiceChannel) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+    }
+}
+}
+}
+client.on("voiceStateUpdate", (message, new1) => {
+
+  var channel = "433738932166066192";
+  var role = "Voice"
+  set(message,new1,channel,role);
+});
+
+function set(o,n,channel,role){
+  if (!o.voiceChannel && n.voiceChannel) {
+    if (n.voiceChannelID == channel) {
+        n.addRole(n.guild.roles.find("name", role));
+    };
+
+  } else if (o.voiceChannel && !n.voiceMove) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+
+
+
+} else if (o.voiceChannel && !n.voiceChannel) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+    }
+}
+}
+}
+client.on("voiceStateUpdate", (message, new1) => {
+
+  var channel = "435510621618962442";
+  var role = "Music 🎶"
+  set(message,new1,channel,role);
+});
+
+function set(o,n,channel,role){
+  if (!o.voiceChannel && n.voiceChannel) {
+    if (n.voiceChannelID == channel) {
+        n.addRole(n.guild.roles.find("name", role));
+    };
+
+  } else if (o.voiceChannel && !n.voiceMove) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+
+
+
+} else if (o.voiceChannel && !n.voiceChannel) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+    }
+}
+}
+}
+
+
+client.on("voiceStateUpdate", (message, new1) => {
+
+  var channel = "483682002151931935";
+  var role = "Gay 🎀"
+  set(message,new1,channel,role);
+});
+
+function set(o,n,channel,role){
+  if (!o.voiceChannel && n.voiceChannel) {
+    if (n.voiceChannelID == channel) {
+        n.addRole(n.guild.roles.find("name", role));
+    };
+
+  } else if (o.voiceChannel && !n.voiceMove) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+
+
+
+} else if (o.voiceChannel && !n.voiceChannel) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+    }
+}
+}
+}
+client.on("voiceStateUpdate", (message, new1) => {
+
+  var channel = "435510792452833280";
+  var role = "[MR] Squad🎤"
+  set(message,new1,channel,role);
+});
+
+function set(o,n,channel,role){
+  if (!o.voiceChannel && n.voiceChannel) {
+    if (n.voiceChannelID == channel) {
+        n.addRole(n.guild.roles.find("name", role));
+    };
+
+  } else if (o.voiceChannel && !n.voiceMove) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+
+
+
+} else if (o.voiceChannel && !n.voiceChannel) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+    }
+}
+}
+}
+
+client.on("voiceStateUpdate", (message, new1) => {
+
+  var channel = "462457162061578240";
+  var role = "Quran☪"
+  set(message,new1,channel,role);
+});
+
+function set(o,n,channel,role){
+  if (!o.voiceChannel && n.voiceChannel) {
+    if (n.voiceChannelID == channel) {
+        n.addRole(n.guild.roles.find("name", role));
+    };
+
+  } else if(o.voiceOld.voiceChannelID !== o.voiceChannelID && o.voiceChannel && o.voiceChannel != null) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+
+
+
+
+} else if (o.voiceChannel && !n.voiceChannel) {
+    if (o.voiceChannelID == channel) {
+        n.removeRole(n.guild.roles.find("name", role))
+    }
+}
+}
+}
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
